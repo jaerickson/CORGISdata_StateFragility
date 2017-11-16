110ab9cb3d57b0386a6cb93696fd6f2b7aa68b65
jaerickson <31251673+jaerickson@users.noreply.github.com>
2017-11-16 09:34:47 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,33 +4,18 @@ import json
 
 app = Flask(__name__)
 
-def get_state_options():
-    with open('county_demographics.json') as demographics_data:
-  counties = json.load(demographics_data)
+def get_country_options():
+    with open('state_fragility (1).json') as demographics_data:
+  countries = json.load(demographics_data)
     options = ""
     s = []
-    for c in counties:
-        if not c["State"] in s:
-            s.append(c["State"])
-            options += Markup("<option value=\"" + c["State"] + "\">" + c["State"] + "</option>")
+    for c in countries:
+        if not c["Country"] in s:
+            s.append(c["Country"])
+            options += Markup("<option value=\"" + c["Country"] + "\">" + c["Country"] + "</option>")
     return options
-def state_fun_fact(states):
-    with open('county_demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
-    s = counties[0]["State"]
-    d = counties[0]["County"]
-    
-    for c in counties:
-        if c["State"] == states:
-            most_women_owned = counties[0]["Employment"]["Firms"]["Women-Owned"]
-            if c["Employment"]["Firms"]["Women-Owned"] > most_women_owned:
-                most_women_owned = c["Employment"]["Firms"]["Women-Owned"]
-                s = c["State"]
-                d = c["County"]
-    return d +" is the highest percentage women owned of "+ s +" "+ str(most_women_owned) +"% women owned"
 @app.route("/")
 def render_home():   
-    return render_template('home.html',states=get_state_options())
-    return render_template('home.html',fact=state_fun_fact(request.args(get_state_options())))
+    return render_template('home.html',countries=get_country_options())
 if __name__ == '__main__':
     app.run(debug=False, port=54321)
