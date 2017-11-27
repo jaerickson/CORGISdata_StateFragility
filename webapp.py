2626adc0db22890a2ecaaa36ec00d1f63e9f47f7
jaerickson <jessica93109@gmail.com>
2017-11-27 10:00:37 -0800

    c

--- webapp.py
+++ webapp.py
@@ -14,14 +14,26 @@ def get_country_options():
             s.append(c["Country"])
             options += Markup("<option value=\"" + c["Country"] + "\">" + c["Country"] + "</option>")
     return options
+def get_legitimacy_score(options):
+    with open('state_fragility (1).json') as demographics_data:
+        countries = json.load(demographics_data)
+	legscore= ""
+    for c in counrties:
+        if c["Country"] == options:
+			legscore=c
+            
 @app.route("/")
 def render_main():
     return render_template('index.html')
 @app.route("/l")
-def render_legitimacy():   
-    return render_template('legitimacy.html',countries=get_country_options())
+def render_legitimacy():
+	#if(type=="submit"):
+		#security=str("security score")
+	#else:
+		#security=str("not submittted")
+    return render_template('legitimacy.html',countries=get_country_options(),security)
 @app.route("/e")
 def render_effectiveness():   
     return render_template('effectiveness.html',countries=get_country_options())
 if __name__ == '__main__':
-    app.run(debug=False, port=54321)
+    app.run(debug=True, port=54321)
